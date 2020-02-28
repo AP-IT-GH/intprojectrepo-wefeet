67509e2602b8b23e22ca0e9749e4e813489cf844
Tom Baeten <tom.baeten048@gmail.com>
2020-02-28 19:30:32 +0100

    Functional quit button

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,4 +9,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("DanceGameScene");
     }
+
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
 }
