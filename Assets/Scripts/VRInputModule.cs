e5f9ef2f7102b342d49c1f544b63816410468547
Tom Baeten <tom.baeten048@gmail.com>
2020-03-04 22:48:08 +0100

    Canvas pointer tutorial 2/3 = done
    Pointing on buttons, no clickable actions yet

--- Assets/Scripts/VRInputModule.cs
+++ Assets/Scripts/VRInputModule.cs
@@ -1,18 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using Valve.VR;
 
-public class VRInputModule : MonoBehaviour
+public class VRInputModule : BaseInputModule
 {
-    // Start is called before the first frame update
-    void Start()
+    public Camera m_Camera;
+    public SteamVR_Input_Sources m_TargetSource;
+    public SteamVR_Action_Boolean m_ClickAction;
+
+    private GameObject m_CurrentObject = null;
+    private PointerEventData m_Data = null;
+
+    protected override void Awake()
     {
-        
+        base.Awake();
+
+        m_Data = new PointerEventData(eventSystem);
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Process()
     {
-        
+        // Reset data, set camera
+        m_Data.Reset();
+        m_Data.position = new Vector2(m_Camera.pixelWidth / 2, m_Camera.pixelHeight / 2);
+
+        // Raycast
+        eventSystem.RaycastAll(m_Data, m_RaycastResultCache);
+        m_Data.pointerCurrentRaycast = FindFirstRaycast(m_RaycastResultCache);
+        m_CurrentObject = m_Data.pointerCurrentRaycast.gameObject;
+
+        // Clear
+        m_RaycastResultCache.Clear();
+
+        // Hover
+        HandlePointerExitAndEnter(m_Data, m_CurrentObject);
+
+        // Press
+        if (m_ClickAction.GetStateDown(m_TargetSource))
+            ProcessPress(m_Data);
+
+        // Release
+        if (m_ClickAction.GetStateUp(m_TargetSource))
+            ProcessRelease(m_Data);
+    }
+
+    public PointerEventData GetData()
+    {
+        return m_Data;
+    }
+
+    private void ProcessPress(PointerEventData data)
+    {
+
+    }
+
+    private void ProcessRelease(PointerEventData data)
+    {
+
     }
 }
