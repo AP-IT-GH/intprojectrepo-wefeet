cb1ee47403fdca9872fb2e21452e2f37c79f0d02
Tom Baeten <tom.baeten048@gmail.com>
2020-03-04 22:28:27 +0100

    Canvas pointer tutorial 1/3 = done
    Only visual pointer. No menu interaction!

---
+++ Assets/Scripts/Pointer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+
+    public float m_DefaultLength = 0.5f;
+    public GameObject m_Dot;
+    public VRInputModule m_InputModule;
+
+    private LineRenderer m_LineRender = null;
+
+    private void Awake()
+    {
+        m_LineRender = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        UpdateLine();
+    }
+
+    private void UpdateLine()
+    {
+        // Use default or distance
+        float targetLength = m_DefaultLength;
+
+        // Raycast
+        RaycastHit hit = CreateRaycast(targetLength);
+
+        // Default
+        Vector3 endPosition = transform.position + (transform.forward * targetLength);
+
+        // Or based on hit
+        if (hit.collider != null)
+            endPosition = hit.point;
+
+        // Set position of the dot
+        m_Dot.transform.position = endPosition;
+
+        // Set position of linerenderer
+        m_LineRender.SetPosition(0, transform.position);
+        m_LineRender.SetPosition(1, endPosition);
+    }
+
+    private RaycastHit CreateRaycast(float length)
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        Physics.Raycast(ray, out hit, m_DefaultLength);
+
+        return hit;
+    }
+}
