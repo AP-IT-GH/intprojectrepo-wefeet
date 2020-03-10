f8f05bb74f7f8a2891ada11fe9cf6c50fa50357e
Kevin van Driel <kevin.yarod.v3l@gmail.com>
2020-03-10 17:28:52 +0100

    rgb script (NO LIGHT EFFECT)

--- Assets/Scripts/RGB.cs
+++ Assets/Scripts/RGB.cs
@@ -4,15 +4,77 @@ using UnityEngine;
 
 public class RGB : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //https://www.youtube.com/watch?v=9z_4hYdN1og
+
+    [Range(0.0f, 1)]
+    public float r;
+
+    [Range(0.0f, 1.0f)]
+    public float g;
+
+    [Range(0.0f, 1.0f)]
+    public float b;
+
+    [Range(0.0f, 1.0f)]
+    public float a;
+
+    [Range(0.0f, 50.0f)]
+    public float blinkSpeed;
+
+    public bool blink = false;
+    public bool random = false;
+    private bool needRandom = false;
+
+    private float r2;
+    private float g2;
+    private float b2;
+    private float count = 0;
+
+    //Cache
+    private Renderer renderCache = null;
+
+    //Use this for initialization
     void Start()
     {
-        
+        a = 1.0f;
+        blinkSpeed = 10.0f;
+        newRandom();
+        renderCache = gameObject.GetComponent<Renderer>();
+        renderCache.material.shader = Shader.Find("Transparent/Diffuse");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (blink == true && count >= 1)
+        {
+            random = false;
+            renderCache.material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), a);
+            count = 0;
+            needRandom = false;
+        }
+        else if(random == true)
+        {
+            if (needRandom == false)
+            {
+                newRandom();
+            }
+            blink = false;
+            needRandom = true;
+            renderCache.material.color = new Color(r2, g2, b2, a);
+        }
+        else if(blink == false && random == false)
+        {
+            renderCache.material.color = new Color(r, g, b, a);
+            needRandom = false;
+        }
+        count += Time.deltaTime * blinkSpeed;
+    }
+
+    void newRandom()
+    {
+        r2 = Random.Range(0.0f, 1.0f);
+        g2 = Random.Range(0.0f, 1.0f);
+        b2 = Random.Range(0.0f, 1.0f);
     }
 }
