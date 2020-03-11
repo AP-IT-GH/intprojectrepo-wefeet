de7b0763ca0d99d5ca630eac0df339af78c3d657
Robin <Huumonen>
2020-03-11 19:42:29 +0200

    Quick fix to LEDs

--- Assets/Scripts/danceMatArduinoCode/danceMatArduinoCode.ino
+++ Assets/Scripts/danceMatArduinoCode/danceMatArduinoCode.ino
@@ -14,257 +14,258 @@ float voltageScaler = 4.6; // Volts
 int pressThreshold = 100; // Ohms
 String stringOutBuffer = String(sectionState[0])+String(sectionState[1])+String(sectionState[2])+String(sectionState[3])+String(sectionState[4])+String(sectionState[5])+String(sectionState[6])+String(sectionState[7])+String(sectionState[8])+",";
 int clearLEDs = 0;
-void setup() 
+void setup()
 {
-  Serial.begin(9600);
-  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+    Serial.begin(9600);
+    FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
 }
 
-void loop() 
+void loop()
 {
-  String stringOut = String(sectionState[0])+String(sectionState[1])+String(sectionState[2])+String(sectionState[3])+String(sectionState[4])+String(sectionState[5])+String(sectionState[6])+String(sectionState[7])+String(sectionState[8])+",";
-  getStates();
-  //getDummyStates();
-  // print the results to the Serial Monitor:
-  if (stringOut != stringOutBuffer)
-  {
-    Serial.print(stringOut);
-    setLEDs();
-  }
-  stringOutBuffer = stringOut;
-  // wait 2 milliseconds before the next loop for the analog-to-digital
-  // converter to settle after the last reading:
-  delay(2);
+    String stringOut = String(sectionState[0])+String(sectionState[1])+String(sectionState[2])+String(sectionState[3])+String(sectionState[4])+String(sectionState[5])+String(sectionState[6])+String(sectionState[7])+String(sectionState[8])+",";
+    getStates();
+    //getDummyStates();
+    // print the results to the Serial Monitor:
+    if (stringOut != stringOutBuffer)
+    {
+        Serial.print(stringOut);
+        setLEDs();
+    }
+    stringOutBuffer = stringOut;
+    // wait 2 milliseconds before the next loop for the analog-to-digital
+    // converter to settle after the last reading:
+    delay(2);
 }
 void getDummyStates ()
 {
-  for (int i = 0; i < sections; i++)
-    sectionState[i] = random(0,2);
+    for (int i = 0; i < sections; i++)
+        sectionState[i] = random(0,2);
 }
 
-void getStates() 
+void getStates()
 {
-  for (int i = 0; i < sections; i++) 
-  {
-    sectionRaw[i] = analogRead(sectionPins[i])*voltageScaler/1024.00;
-    sectionBuffer[i] = (voltageScaler/sectionRaw[i]) - 1;
-    sectionRaw[i] = resistor*sectionBuffer[i];
-    if (sectionRaw[i] <= pressThreshold)
-      sectionState[i] = 1;
+    for (int i = 0; i < sections; i++)
+    {
+        sectionRaw[i] = analogRead(sectionPins[i])*voltageScaler/1024.00;
+        sectionBuffer[i] = (voltageScaler/sectionRaw[i]) - 1;
+        sectionRaw[i] = resistor*sectionBuffer[i];
+        if (sectionRaw[i] <= pressThreshold)
+            sectionState[i] = 1;
+        else
+            sectionState[i] = 0;
+    }
+}
+void setLEDs()
+{
+    int offset = 0;
+
+    if (sectionState[0])
+    {
+        offset = 0;
+        for (int i = 0 + offset; i < 2*LEDsPerSection + offset; i++)
+        {
+            int r = random(0, 256);
+            int g = random(0, 256);
+            int b = random(0, 256);
+            leds[i] = CRGB(r, g, b);
+            FastLED.show();
+        }
+
+    }
     else
-      sectionState[i] = 0;
-  }
-}
-void setLEDs() 
-{
-    
-int sectionState1 = sectionState[0];
-int sectionState2 = sectionState[1];
-int sectionState3 = sectionState[2];
-int sectionState4 = sectionState[3];
-int sectionState5 = sectionState[4];
-int sectionState6 = sectionState[5];
-int sectionState7 = sectionState[6];
-int sectionState8 = sectionState[7];
-int sectionState9 = sectionState[8];
-int offset = 0;
-
-    if (sectionState1)
     {
-      offset = 0;
-      for (int i = 0 + offset; i < LEDsPerSection + offset; i++) 
-      {
-        int r = random(0, 256);
-        int g = random(0, 256);
-        int b = random(0, 256);
-        leds[i] = CRGB(r, g, b);
-        FastLED.show();
-      }
-      
-    }
-    else 
-    {
-      for (int i = 0; i < LEDsPerSection; i++)
-      {
-        leds[i] = CRGB(0, 0, 0);
-        FastLED.show();
-      }
+        for (int i = 0 + offset; i < 2*LEDsPerSection + offset; i++)
+        {
+            leds[i] = CRGB(0, 0, 0);
+            FastLED.show();
+        }
     }
 
-    if (sectionState2)
+    if (sectionState[1])
     {
-      offset = offset + 30;
-      for (int i = 0 + offset; i < LEDsPerSection + offset; i++) 
-      {
-        int r = random(0, 256);
-        int g = random(0, 256);
-        int b = random(0, 256);
-        leds[i] = CRGB(r, g, b);
-        FastLED.show();
-      }
-      
+        offset = 60;
+        for (int i = 0 + offset; i < LEDsPerSection + offset; i++)
+        {
+            int r = random(0, 256);
+            int g = random(0, 256);
+            int b = random(0, 256);
+            leds[i] = CRGB(r, g, b);
+            FastLED.show();
+        }
+
     }
-    else 
+    else
     {
-      for (int i = 0; i < LEDsPerSection; i++)
-      {
-        leds[i] = CRGB(0, 0, 0);
-        FastLED.show();
-      }
+        for (int i = 0 + offset; i < LEDsPerSection + offset; i++)
+        {
+            leds[i] = CRGB(0, 0, 0);
+            FastLED.show();
+        }
     }
 
-    if (sectionState3)
+    if (sectionState[2])
     {
-      offset = offset + 30*2;
-      for (int i = 0 + offset; i < LEDsPerSection + offset; i++) 
-      {
-        int r = random(0, 256);
-        int g = random(0, 256);
-        int b = random(0, 256);
-        leds[i] = CRGB(r, g, b);
-        FastLED.show();
-      }
-      
+        offset = 90;
+        for (int i = 0 + offset; i < 2*LEDsPerSection + offset; i++)
+        {
+            int r = random(0, 256);
+            int g = random(0, 256);
+            int b = random(0, 256);
+            leds[i] = CRGB(r, g, b);
+            FastLED.show();
+        }
+
     }
-    else 
+    else
     {
-      for (int i = 0; i < LEDsPerSection; i++)
-      {
-        leds[i] = CRGB(0, 0, 0);
-        FastLED.show();
-      }
+        for (int i = 0 + offset; i < 2*LEDsPerSection + offset; i++)
+        {
+            leds[i] = CRGB(0, 0, 0);
+            FastLED.show();
+        }
     }
 
-    if (sectionState4)
+    if (sectionState[3])
     {
-      offset = offset + 30;
-      for (int i = 0 + offset; i < LEDsPerSection + offset; i++) 
-      {
-        int r = random(0, 256);
-        int g = random(0, 256);
-        int b = random(0, 256);
-        leds[i] = CRGB(r, g, b);
-        FastLED.show();
-      }
-      
+        offset = 300;
+        for (int i = 0 + offset; i < LEDsPerSection + offset; i++)
+        {
+            int r = random(0, 256);
+            int g = random(0, 256);
+            int b = random(0, 256);
+            leds[i] = CRGB(r, g, b);
+            FastLED.show();
+        }
+
     }
-    else 
+    else
     {
-      for (int i = 0; i < LEDsPerSection; i++)
-      {
-        leds[i] = CRGB(0, 0, 0);
-        FastLED.show();
-      }
+        for (int i = 0 + offset; i < LEDsPerSection + offset; i++)
+        {
+            leds[i] = CRGB(0, 0, 0);
+            FastLED.show();
+        }
     }
 
-    if (sectionState5)
+    if (sectionState[4])
     {
-      offset = offset + 30*2;
-      for (int i = 0 + offset; i < LEDsPerSection + offset; i++) 
-      {
-        int r = random(0, 256);
-        int g = random(0, 256);
-        int b = random(0, 256);
-        leds[i] = CRGB(r, g, b);
-        FastLED.show();
-      }
-      
+        for (int i = 0; i < LEDsPerSection; i++)
+        {
+            int r = random(0, 256);
+            int g = random(0, 256);
+            int b = random(0, 256);
+            leds[i+60] = CRGB(r, g, b);
+            FastLED.show();
+            leds[i+150] = CRGB(r, g, b);
+            FastLED.show();
+            leds[i+240] = CRGB(r, g, b);
+            FastLED.show();
+            leds[i+300] = CRGB(r, g, b);
+            FastLED.show();
+        }
+
     }
-    else 
+    else
     {
-      for (int i = 0; i < LEDsPerSection; i++)
-      {
-        leds[i] = CRGB(0, 0, 0);
-        FastLED.show();
-      }
+        for (int i = 0; i < LEDsPerSection; i++)
+        {
+            leds[i+60] = CRGB(0, 0, 0);
+            FastLED.show();
+            leds[i+150] = CRGB(0, 0, 0);
+            FastLED.show();
+            leds[i+240] = CRGB(0, 0, 0);
+            FastLED.show();
+            leds[i+300] = CRGB(0, 0, 0);
+            FastLED.show();
+        }
     }
 
-    if (sectionState6)
+    if (sectionState[5])
     {
-      offset = offset + 30;
-      for (int i = 0 + offset; i < LEDsPerSection + offset; i++) 
-      {
-        int r = random(0, 256);
-        int g = random(0, 256);
-        int b = random(0, 256);
-        leds[i] = CRGB(r, g, b);
-        FastLED.show();
-      }
-      
+        offset = 150;
+        for (int i = 0 + offset; i < LEDsPerSection + offset; i++)
+        {
+            int r = random(0, 256);
+            int g = random(0, 256);
+            int b = random(0, 256);
+            leds[i] = CRGB(r, g, b);
+            FastLED.show();
+        }
+
     }
-    else 
+    else
     {
-      for (int i = 0; i < LEDsPerSection; i++)
-      {
-        leds[i] = CRGB(0, 0, 0);
-        FastLED.show();
-      }
+        for (int i = 0 + offset; i < LEDsPerSection + offset; i++)
+        {
+            leds[i] = CRGB(0, 0, 0);
+            FastLED.show();
+        }
     }
 
-    if (sectionState7)
+    if (sectionState[6])
     {
-      offset = offset + 30*2;
-      for (int i = 0 + offset; i < LEDsPerSection + offset; i++) 
-      {
-        int r = random(0, 256);
-        int g = random(0, 256);
-        int b = random(0, 256);
-        leds[i] = CRGB(r, g, b);
-        FastLED.show();
-      }
-      
+        offset = 270;
+        for (int i = 0 + offset; i < 2*LEDsPerSection + offset; i++)
+        {
+            int r = random(0, 256);
+            int g = random(0, 256);
+            int b = random(0, 256);
+            leds[i] = CRGB(r, g, b);
+            FastLED.show();
+        }
+
     }
-    else 
+    else
     {
-      for (int i = 0; i < LEDsPerSection; i++)
-      {
-        leds[i] = CRGB(0, 0, 0);
-        FastLED.show();
-      }
+        for (int i = 0 + offset; i < LEDsPerSection + offset; i++)
+        {
+            leds[i] = CRGB(0, 0, 0);
+            FastLED.show();
+        }
     }
 
-    if (sectionState8)
+    if (sectionState[7])
     {
-      offset = offset + 30;
-      for (int i = 0 + offset; i < LEDsPerSection + offset; i++) 
-      {
-        int r = random(0, 256);
-        int g = random(0, 256);
-        int b = random(0, 256);
-        leds[i] = CRGB(r, g, b);
-        FastLED.show();
-      }
-      
+        offset = 240;
+        for (int i = 0 + offset; i < LEDsPerSection + offset; i++)
+        {
+            int r = random(0, 256);
+            int g = random(0, 256);
+            int b = random(0, 256);
+            leds[i] = CRGB(r, g, b);
+            FastLED.show();
+        }
+
     }
-    else 
+    else
     {
-      for (int i = 0; i < LEDsPerSection; i++)
-      {
-        leds[i] = CRGB(0, 0, 0);
-        FastLED.show();
-      }
+        for (int i = 0 + offset; i < LEDsPerSection + offset; i++)
+        {
+            leds[i] = CRGB(0, 0, 0);
+            FastLED.show();
+        }
     }
 
-    if (sectionState9)
+    if (sectionState[8])
     {
-      offset = offset + 30*2;
-      for (int i = 0 + offset; i < LEDsPerSection + offset; i++) 
-      {
-        int r = random(0, 256);
-        int g = random(0, 256);
-        int b = random(0, 256);
-        leds[i] = CRGB(r, g, b);
-        FastLED.show();
-      }
-      
+        offset = 180;
+        for (int i = 0 + offset; i < 2*LEDsPerSection + offset; i++)
+        {
+            int r = random(0, 256);
+            int g = random(0, 256);
+            int b = random(0, 256);
+            leds[i] = CRGB(r, g, b);
+            FastLED.show();
+        }
+
     }
-    else 
+    else
     {
-      for (int i = 0; i < LEDsPerSection; i++)
-      {
-        leds[i] = CRGB(0, 0, 0);
-        FastLED.show();
-      }
+        for (int i = 0 + offset; i < 2*LEDsPerSection + offset; i++)
+        {
+            leds[i] = CRGB(0, 0, 0);
+            FastLED.show();
+        }
     }
-    
+
 }
