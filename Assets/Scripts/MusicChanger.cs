69258ab21ad2bf036ea0cbacbb4e908adc951a7d
Kevin van Driel <kevin.yarod.v3l@gmail.com>
2020-03-16 17:06:12 +0100

    added music

---
+++ Assets/Scripts/MusicChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicChanger : MonoBehaviour
+{
+    public AudioClip[] ricochet; // The array controlling the sounds
+    public int rayMax; // The max amount of Sounds there are
+    public int pickedSound; // The sound is choose to play
+    // Use this for initialization
+    void Start()
+    {
+        pickedSound = Random.Range(0, rayMax); // Grab a random sound out of the max
+        gameObject.GetComponent<AudioSource>().clip = ricochet[pickedSound]; // Tell the AudioSource to become that sound
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
