041b8e6d6c913e74a17598438bee8c86c01f4b24
Lennert Van Riel <lennert.vanriel@hotmail.be>
2020-03-27 15:06:14 +0100

    added the first song

--- Assets/Scripts/CheckMatMoveCinema.cs
+++ Assets/Scripts/CheckMatMoveCinema.cs
@@ -14,6 +14,7 @@ public class CheckMatMoveCinema : MonoBehaviour
     public GameObject[] tiles;
 
     public int Score { get => score; }
+    private Move.moveResponse requiredMove;
 
 
     void Start()
@@ -31,8 +32,13 @@ public class CheckMatMoveCinema : MonoBehaviour
 
         ShowFeet();
 
+        #region template
+        /*requiredMove = Move.Moves(currentMove, *movenr*, currentTime, *begintime*, *endtime*, Mat, *string move*);
+        checkMove(requiredMove);*/
+        #endregion
+
         #region both feet center at 0 seconds
-        //3 seconds time to complete the move
+        /*//3 seconds time to complete the move
         if (currentTime < 3)
         {
             if(currentMove == 0)
@@ -50,28 +56,112 @@ public class CheckMatMoveCinema : MonoBehaviour
                 IncorrectMove();
             }
         }
+        //OR
+        requiredMove = Move.Moves(currentMove, 1, currentTime, 0, 3, Mat);
+        switch (requiredMove)
+        {
+            case Move.moveResponse.Correct:
+                CorrectMove();
+                break;
+            case Move.moveResponse.Incorrect:
+                IncorrectMove();
+                break;
+            default:
+                //waiting, do nothing
+                break;
+        }*/
         #endregion
 
-        #region 1 foot left and 1 foot right at 10 seconds
-        //3 seconds time to complete the move
-        if (currentTime > 9 && currentTime < 12)
-        {
-            if(currentMove == 1)
-            {
-                if(Mat.Left && Mat.Right)
-                {
-                    CorrectMove();
-                }
-            }
-        }
-        else
-        {
-            if (currentMove == 1 && currentTime > 12)
-            {
-                IncorrectMove();
-            }
-        }
+        #region 0
+        requiredMove = Move.Moves(currentMove, 0, currentTime, 7, 9, Mat, "101000000");
+        checkMove(requiredMove);
         #endregion
+
+        #region 1
+        requiredMove = Move.Moves(currentMove, 1, currentTime, 9, 11, Mat, "000101000");
+        checkMove(requiredMove);
+        #endregion
+
+        #region 2
+        requiredMove = Move.Moves(currentMove, 2, currentTime, 11, 14, Mat, "000000101");
+        checkMove(requiredMove);
+        #endregion
+
+        #region 3
+        requiredMove = Move.Moves(currentMove, 3, currentTime, 14, 22, Mat, "000010000");
+        checkMove(requiredMove);
+        #endregion
+
+        #region 4
+        requiredMove = Move.Moves(currentMove, 4, currentTime, 22, 24, Mat, "000100000");
+        checkMove(requiredMove);
+        #endregion
+
+        #region 5
+        requiredMove = Move.Moves(currentMove, 5, currentTime, 24, 28, Mat, "000010000");
+        checkMove(requiredMove);
+        #endregion
+
+        #region 6
+        requiredMove = Move.Moves(currentMove, 6, currentTime, 28, 30, Mat, "000011000");
+        checkMove(requiredMove);
+        #endregion
+
+        #region 7
+        requiredMove = Move.Moves(currentMove, 7, currentTime, 30, 32, Mat, "000110000");
+        checkMove(requiredMove);
+        #endregion
+
+        #region 8
+        requiredMove = Move.Moves(currentMove, 8, currentTime, 32, 33, Mat, "000010000");
+        checkMove(requiredMove);
+        #endregion
+
+        #region 9
+        requiredMove = Move.Moves(currentMove, 9, currentTime, 33, 34, Mat, "101000000");
+        checkMove(requiredMove);
+        #endregion
+
+        #region 10
+        requiredMove = Move.Moves(currentMove, 10, currentTime, 34, 35, Mat, "000101000");
+        checkMove(requiredMove);
+        #endregion
+
+        #region 11
+        requiredMove = Move.Moves(currentMove, 11, currentTime, 35, 36, Mat, "000000101");
+        checkMove(requiredMove);
+        #endregion
+
+        #region 12
+        requiredMove = Move.Moves(currentMove, 12, currentTime, 36, 38, Mat, "000010000");
+        checkMove(requiredMove);
+        #endregion
+
+        #region 13
+        requiredMove = Move.Moves(currentMove, 13, currentTime, 38, 39, Mat, "000100000");
+        checkMove(requiredMove);
+        #endregion
+
+        #region 14
+        requiredMove = Move.Moves(currentMove, 14, currentTime, 39, 42, Mat, "000010000");
+        checkMove(requiredMove);
+        #endregion
+
+        #region 15
+        requiredMove = Move.Moves(currentMove, 15, currentTime, 42, 44, Mat, "000011000");
+        checkMove(requiredMove);
+        #endregion
+
+        #region 16
+        requiredMove = Move.Moves(currentMove, 16, currentTime, 44, 48, Mat, "000110000");
+        checkMove(requiredMove);
+        #endregion
+
+        #region 17
+        requiredMove = Move.Moves(currentMove, 17, currentTime, 48, 53, Mat, "000010000");
+        checkMove(requiredMove);
+        #endregion
+
     }
 
     private void ShowFeet() //visually show where your feet are
@@ -90,6 +180,21 @@ public class CheckMatMoveCinema : MonoBehaviour
         if (Mat.Backward) tiles[7].SetActive(true);
         if (Mat.RightBackward) tiles[8].SetActive(true);
     }
+    private void checkMove(Move.moveResponse requiredMove)
+    {
+        switch (requiredMove)
+        {
+            case Move.moveResponse.Correct:
+                CorrectMove();
+                break;
+            case Move.moveResponse.Incorrect:
+                IncorrectMove();
+                break;
+            default:
+                //waiting, do nothing
+                break;
+        }
+    }
 
     private void IncorrectMove()
     {
@@ -102,5 +207,62 @@ public class CheckMatMoveCinema : MonoBehaviour
         score++;
         currentMove++;
         Debug.Log("SCORE: " + score);
+    }
+}
+
+
+public static class Move
+{
+    public enum moveResponse
+    {
+        Correct,
+        Incorrect,
+        Waiting
+    }
+
+    public static moveResponse Moves(int currentMove, int moveNumber, float currentTime, float beginTime, float endTime, MatInputController Mat, string requiredMove)
+    {
+        if (currentTime > beginTime && currentTime < endTime)
+        {
+            if (currentMove == moveNumber)
+            {
+                List<bool> moves = new List<bool>();
+                foreach (char item in requiredMove)
+                {
+                    moves.Add(Convert.ToBoolean(item));
+                }
+                if (Mat.LeftForward == moves[0] && 
+                    Mat.Forward == moves[1] && 
+                    Mat.RightForward == moves[2] && 
+                    Mat.Left == moves[3] &&
+                    Mat.Center == moves[4] &&
+                    Mat.Right == moves[5] &&
+                    Mat.LeftBackward == moves[6] &&
+                    Mat.Backward == moves[7] &&
+                    Mat.RightBackward == moves[8])
+                {
+                    return moveResponse.Correct;
+                }
+                else
+                {
+                    return moveResponse.Waiting; //allowed to make the move but havent made it yet
+                }
+            }
+            else
+            {
+                return moveResponse.Waiting;
+            }
+        }
+        else
+        {
+            if (currentMove == moveNumber && currentTime > endTime)
+            {
+                return moveResponse.Incorrect;
+            }
+            else
+            {
+                return moveResponse.Waiting;
+            }
+        }
     }
 }
