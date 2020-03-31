08fb75b5a3af7fd879e1a92e34e4ba5c33b7c4bb
QuintenS <quinten.schutyser@gmail.com>
2020-03-31 14:38:50 +0200

    Add MusicManager

---
+++ Assets/Scripts/Audio/AudioManagerCheck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManagerCheck : MonoBehaviour
+{
+    public GameObject AudioManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (FindObjectOfType<AudioManager>())
+            return;
+        else
+            Instantiate(AudioManager, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
