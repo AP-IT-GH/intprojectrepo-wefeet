5598cbee57a1100d40fe55d8151c287a0fa6d380
QuintenS <quinten.schutyser@gmail.com>
2020-03-31 15:19:00 +0200

    New AudioListener
    deleted old Audio Listeners and making the new listener

---
+++ Assets/Scripts/Audio/SetVolume.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer Mixer;
+
+    public void SetLevel(float sliderValue)
+    {
+        //Mixer.SetFloat("MusicVol", sliderValue); //Je moet de float nog omvormen naar een logaritmise waarde
+        Mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue)*20);
+
+    }
+}
