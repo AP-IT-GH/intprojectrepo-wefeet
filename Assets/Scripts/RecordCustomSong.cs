919d6ffe0a996b7735ff62ab1dc2b770f4fd1652
Tom Baeten <tom.baeten048@gmail.com>
2020-05-16 12:28:51 +0200

    UI input

--- Assets/Scripts/RecordCustomSong.cs
+++ Assets/Scripts/RecordCustomSong.cs
@@ -2,30 +2,66 @@
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using Valve.VR;
 
 public class RecordCustomSong : MonoBehaviour
 {
+    // MAT
     private MatInputController Mat = new MatInputController();
 
+    // CSV
     public string csvName = "test.csv";
     public string song { get; set; }
 
+    // Moves
     List<CustomMove> moves = new List<CustomMove>();
+
+    // UI Input    
+    public SteamVR_Action_Boolean RecordTrigger;    
+    public SteamVR_Input_Sources handType;
+    private bool TriggerOnOff = false;
+
+    // Timings
+    private float prevTime = 0;
+    private float currTime = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        csvName = "Dance-" + song;
+        RecordTrigger.AddOnStateDownListener(TriggerDown, handType);
+        RecordTrigger.AddOnStateUpListener(TriggerUp, handType);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RecordTrigger.AddOnStateDownListener(TriggerDown, handType);
+        RecordTrigger.AddOnStateUpListener(TriggerUp, handType);
+    }
+
+    public void TriggerUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        //selectedGameObject.SetActive(false);
+        if (TriggerOnOff)
+            TriggerOnOff = false;
+    }
+
+    public void TriggerDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        //selectedGameObject.SetActive(true);
+        //Debug.Log("change menu");
+        if (!TriggerOnOff)
+        {
+            TriggerOnOff = true;
+            //  Add new move
+            moves.Add(new CustomMove(prevTime, currTime, "000000000")); // TODO: Change by input mat
+        }
     }
 
     private void WriteCsv()
     {
+        csvName = "Dance-" + song;
+
         StreamWriter writer = new StreamWriter(@"songScripts\" + csvName);
 
         writer.WriteLine(song + ";;"); //First line = songname
