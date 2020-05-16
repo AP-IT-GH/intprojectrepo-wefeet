1d99ca7f6cb12f73fae1a1c8a8b1869d9756d7b6
QuintenS <quinten.schutyser@gmail.com>
2020-05-16 12:59:28 +0200

    Add MenuInteraction Button

--- Assets/SteamVR_Input/SteamVR_Input_Actions.cs
+++ Assets/SteamVR_Input/SteamVR_Input_Actions.cs
@@ -57,6 +57,8 @@ namespace Valve.VR
         
         private static SteamVR_Action_Boolean p_menuButton1_MenuOpen;
         
+        private static SteamVR_Action_Boolean p_menuButton1_MenuInteraction;
+        
         public static SteamVR_Action_Boolean default_InteractUI
         {
             get
@@ -217,6 +219,14 @@ namespace Valve.VR
             }
         }
         
+        public static SteamVR_Action_Boolean menuButton1_MenuInteraction
+        {
+            get
+            {
+                return SteamVR_Actions.p_menuButton1_MenuInteraction.GetCopy<SteamVR_Action_Boolean>();
+            }
+        }
+        
         private static void InitializeActionArrays()
         {
             Valve.VR.SteamVR_Input.actions = new Valve.VR.SteamVR_Action[] {
@@ -239,7 +249,8 @@ namespace Valve.VR
                     SteamVR_Actions.buggy_Brake,
                     SteamVR_Actions.buggy_Reset,
                     SteamVR_Actions.mixedreality_ExternalCamera,
-                    SteamVR_Actions.menuButton1_MenuOpen};
+                    SteamVR_Actions.menuButton1_MenuOpen,
+                    SteamVR_Actions.menuButton1_MenuInteraction};
             Valve.VR.SteamVR_Input.actionsIn = new Valve.VR.ISteamVR_Action_In[] {
                     SteamVR_Actions.default_InteractUI,
                     SteamVR_Actions.default_Teleport,
@@ -259,7 +270,8 @@ namespace Valve.VR
                     SteamVR_Actions.buggy_Brake,
                     SteamVR_Actions.buggy_Reset,
                     SteamVR_Actions.mixedreality_ExternalCamera,
-                    SteamVR_Actions.menuButton1_MenuOpen};
+                    SteamVR_Actions.menuButton1_MenuOpen,
+                    SteamVR_Actions.menuButton1_MenuInteraction};
             Valve.VR.SteamVR_Input.actionsOut = new Valve.VR.ISteamVR_Action_Out[] {
                     SteamVR_Actions.default_Haptic};
             Valve.VR.SteamVR_Input.actionsVibration = new Valve.VR.SteamVR_Action_Vibration[] {
@@ -278,7 +290,8 @@ namespace Valve.VR
                     SteamVR_Actions.platformer_Jump,
                     SteamVR_Actions.buggy_Brake,
                     SteamVR_Actions.buggy_Reset,
-                    SteamVR_Actions.menuButton1_MenuOpen};
+                    SteamVR_Actions.menuButton1_MenuOpen,
+                    SteamVR_Actions.menuButton1_MenuInteraction};
             Valve.VR.SteamVR_Input.actionsSingle = new Valve.VR.SteamVR_Action_Single[] {
                     SteamVR_Actions.default_Squeeze,
                     SteamVR_Actions.buggy_Throttle};
@@ -304,7 +317,8 @@ namespace Valve.VR
                     SteamVR_Actions.buggy_Throttle,
                     SteamVR_Actions.buggy_Brake,
                     SteamVR_Actions.buggy_Reset,
-                    SteamVR_Actions.menuButton1_MenuOpen};
+                    SteamVR_Actions.menuButton1_MenuOpen,
+                    SteamVR_Actions.menuButton1_MenuInteraction};
         }
         
         private static void PreInitActions()
@@ -329,6 +343,7 @@ namespace Valve.VR
             SteamVR_Actions.p_buggy_Reset = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/buggy/in/Reset")));
             SteamVR_Actions.p_mixedreality_ExternalCamera = ((SteamVR_Action_Pose)(SteamVR_Action.Create<SteamVR_Action_Pose>("/actions/mixedreality/in/ExternalCamera")));
             SteamVR_Actions.p_menuButton1_MenuOpen = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/MenuButton1/in/MenuOpen")));
+            SteamVR_Actions.p_menuButton1_MenuInteraction = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/MenuButton1/in/MenuInteraction")));
         }
     }
 }
