abc13ae2ebfd86f269e449cbb21cd67864c367c6
QuintenS <quinten.schutyser@gmail.com>
2020-05-17 00:12:49 +0200

    List to show the songs

---
+++ Assets/Scripts/Menu/SongListButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SongListButton : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI myText;
+    [SerializeField]
+    private SongListController songListController;  
+
+    private string myTextString;
+
+    public void SetText(string textString)
+    {
+        myTextString = textString;
+        myText.text = textString;
+    }
+
+    public void OnClick()
+    {
+        songListController.ButtonClicked(myTextString);
+    }
+}
