5bbdb0549460636987bcde166411c9b36fddba06
QuintenS <quinten.schutyser@gmail.com>
2020-05-27 15:51:52 +0200

    change button array

--- Assets/Scripts/Menu/SongListController.cs
+++ Assets/Scripts/Menu/SongListController.cs
@@ -8,23 +8,7 @@ public class SongListController : MonoBehaviour
 {
     [SerializeField]
     private GameObject buttonTemplate;
-    private string[] TestSongs = new string[]{
-        @"c:\test\test\song.mp3",
-        @"c:\test\test\song1.mp3",
-        @"c:\test\test\song2.mp3",
-        @"c:\test\test\song3.mp3",
-        @"c:\test\test\song4.mp3",
-        @"c:\test\test\song5.mp3",
-        @"c:\test\test\song6.mp3",
-        @"c:\test\test\song7.mp3",
-        @"c:\test\test\song8.mp3",
-        @"c:\test\test\song9.mp3",
-        @"c:\test\test\song10.mp3",
-        @"c:\test\test\song11.mp3",
-        @"c:\test\test\randomLongSongNameToTest.mp3",
-        @"c:\test\test\ChaCha.mp3",
-        @"c:\test\test\Dance_Monkey.mp3",
-    };
+    private ArrayList TestSongs = new ArrayList();    
     private string[] songNames = new string[] { };
 
     public bool makeCostumsSongs = false;
@@ -33,13 +17,17 @@ public class SongListController : MonoBehaviour
 
     void Start()
     {
+        
+
         if (makeCostumsSongs)
-            Debug.Log("make costum song");
-        if (playCostumSongs)
-            Debug.Log("Play Costum song");
-        if(!playCostumSongs && !playCostumSongs)
+            songNames = GetNamesSong(findSongs.MadeSongs());
+        else if (playCostumSongs)
+            songNames = GetNamesSong(findSongs.songsInProject);
+        else if (!playCostumSongs || !playCostumSongs) {
+            initTestArrayList();
             songNames = GetNamesSong(TestSongs);
-        songNames = GetNamesSong(findSongs.songsInProject);
+        }
+
 
         foreach (string song in songNames)
         {
@@ -71,8 +59,17 @@ public class SongListController : MonoBehaviour
         }
         return listSongs.ToArray();
     }
-    void getSongsInFile()
+
+    private void initTestArrayList()
     {
-        //Application.dataPath;
+        TestSongs.Add(@"c:\test\test\song.mp3");
+        TestSongs.Add(@"c:\test\test\song1.mp3");
+        TestSongs.Add(@"c:\test\test\song2.mp3");
+        TestSongs.Add(@"c:\test\test\song3.mp3");
+        TestSongs.Add(@"c:\test\test\song4.mp3");
+        TestSongs.Add(@"c:\test\test\song5.mp3");
+        TestSongs.Add(@"c:\test\test\ChaCha.mp3");
+        TestSongs.Add(@"c:\test\test\Dance_Monkey.mp3");
+        TestSongs.Add(@"c:\test\test\randomLongSongNameToTest.mp3");
     }
 }
